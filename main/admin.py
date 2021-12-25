1fbbcf2320753392ea84a22622be882f8e03c133
Johirul Islam <89728940+Johirul-Islam-JiJ@users.noreply.github.com>
2021-12-25 15:51:46 +0600

    maati

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from main.models import buyer, seller
+
 # Register your models here.
+
+class sellerAdmin(admin.ModelAdmin):
+    list_display = ['name','phone','email','nid','address']
+
+class buyerAdmin(admin.ModelAdmin):
+    list_display=['name','phone','email','nid','address']
+
+
+admin.site.register(seller,sellerAdmin)
+admin.site.register(buyer,buyerAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.db.models.fields import EmailField
 
 # Create your models here.
+
+
+class buyer(models.Model):
+    name = models.CharField(max_length=200,null=False)
+    phone = models.CharField(max_length=15,null=False)
+    email = models.EmailField(null=True)
+    nid = models.CharField(max_length=30,null = False)
+    address = models.CharField(max_length=400,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class seller(models.Model):
+    name = models.CharField(max_length=200,null = False)
+    phone = models.CharField(max_length=15, null=False)
+    email = models.EmailField(null=False)
+    nid = models.CharField(max_length=30,null=False)
+    address = models.CharField(max_length=400,null=False)
+
+    def __str__(self):
+        return self.name
+    
